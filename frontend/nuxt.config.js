1af8851583f23e4832dee25ff33eff7dbb7cc774
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-09 08:24:44 +0800

    chore: added Vuetify module in config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -43,5 +43,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+   // Vuetify module configuration
+   vuetify: {
+    // customVariables: ['~/assets/variables.scss'],
+    // theme: { ... }
+  },
 }
