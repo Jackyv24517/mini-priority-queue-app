5a4337e43c20dccb9f54201dc48c60e6debc0e57
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-09 09:20:17 +0800

    feat: submit order page basic structure

---
+++ frontend/pages/submit-order.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="order-submission-page">
+    <OrderForm />
+  </div>
+  </template>
+  
+  <script>
+  import OrderForm from '~/components/OrderForm';
+
+  export default {
+    
+    components: {
+        OrderForm
+    }
+  }
+  </script>
+  
+  <style scoped></style>
