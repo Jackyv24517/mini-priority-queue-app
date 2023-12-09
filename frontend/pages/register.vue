45dc75c42f7eb3c5efe37c49754caeffe138d07c
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-09 15:49:51 +0800

    fix: resolve vuetify local data-app issue

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,40 +1,42 @@
 <template>
-    <v-container>
-      <v-card>
-        <v-card-title>Register</v-card-title>
-        <v-card-text>
-          <v-form @submit.prevent="registerUser">
-            <v-text-field
-              label="Username"
-              v-model="username"
-              :rules="[rules.required]"
-              required
-            ></v-text-field>
-  
-            <v-text-field
-              label="Password"
-              type="password"
-              v-model="password"
-              :rules="[rules.required]"
-              required
-            ></v-text-field>
-  
-            <v-select
-              label="Role"
-              :items="roles"
-              v-model="role"
-              :rules="[rules.required]"
-              required
-            ></v-select>
-  
-            <v-btn type="submit" color="primary">Register</v-btn>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn text @click="gotoLogin">Already have an account? Login</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-container>
+    <v-app data-app>
+        <v-container>
+        <v-card>
+            <v-card-title>Register</v-card-title>
+            <v-card-text>
+            <v-form @submit.prevent="registerUser">
+                <v-text-field
+                label="Username"
+                v-model="username"
+                :rules="[rules.required]"
+                required
+                ></v-text-field>
+    
+                <v-text-field
+                label="Password"
+                type="password"
+                v-model="password"
+                :rules="[rules.required]"
+                required
+                ></v-text-field>
+    
+                <v-select
+                label="Role"
+                :items="roles"
+                v-model="role"
+                :rules="[rules.required]"
+                required
+                ></v-select>
+    
+                <v-btn type="submit" color="primary">Register</v-btn>
+            </v-form>
+            </v-card-text>
+            <v-card-actions>
+            <v-btn text @click="gotoLogin">Already have an account? Login</v-btn>
+            </v-card-actions>
+        </v-card>
+        </v-container>
+    </v-app>
   </template>
   
   <script>
