7ab117490a89a54881eddeb7470247df9b7965bf
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-09 15:27:00 +0800

    feat: integrate backend login API in login page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -36,24 +36,27 @@
       };
     },
     methods: {
-      async submitLogin() {
-        try {
-          // const response = await axios.post('http://your-backend-url/api/login', this.loginDetails);
-          const { token, role } = response.data;
-  
-          // Handle successful login here
-          
-          // Save the token, perhaps in localStorage or a cookie
-          localStorage.setItem('userToken', token);
-  
-          // Redirect user based on role
-          this.$router.push(role === 'manager' ? '/manager-dashboard' : '/');
-  
-        } catch (error) {
-          // Handle errors here
-          this.error = 'Invalid username or password';
-        }
-      }
+        async submitLogin() {
+            try {
+                // Make an API call to the backend's login endpoint
+                const response = await this.$axios.post('/auth/login', this.loginDetails);
+
+                // Destructure the token and role from the response data
+                const { token, role } = response.data;
+
+                // Handle successful login
+                // Save the token in localStorage
+                localStorage.setItem('userToken', token);
+
+                // Redirect user based on role
+                this.$router.push(role === 'manager' ? '/manager-dashboard' : '/');
+
+            } catch (error) {
+                // Handle errors here
+                // You can customize this error message based on the error response from your backend
+                this.error = error.response && error.response.data ? error.response.data.message : 'Invalid username or password';
+            }
+        },
     }
   };
   </script>
