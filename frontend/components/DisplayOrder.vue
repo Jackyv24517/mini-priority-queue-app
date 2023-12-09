feefdb0753f6f0ea2cd2217acca463baf29c2d58
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-09 09:20:51 +0800

    feat: add display order component basic layout

---
+++ frontend/components/DisplayOrder.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-container>
+      <h2>Pending Orders</h2>
+      <v-list>
+        <v-list-item 
+          v-for="order in pendingOrders" 
+          :key="order.id"
+        >
+          {{ order.details }} - {{ order.type }}
+        </v-list-item>
+      </v-list>
+  
+      <h2>Completed Orders</h2>
+      <v-list>
+        <v-list-item 
+          v-for="order in completedOrders" 
+          :key="order.id"
+        >
+          {{ order.details }} - {{ order.type }}
+        </v-list-item>
+      </v-list>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        pendingOrders: [],
+        completedOrders: []
+      };
+    },
+    mounted() {
+      // Fetch orders from the backend and populate pendingOrders and completedOrders
+
+      // mock template, needed to replace with actual API call
+      this.pendingOrders = [
+        { id: 1, details: 'Pizza', type: 'Normal' },
+        { id: 2, details: 'Burger', type: 'VIP' }
+      ];
+      this.completedOrders = [
+        { id: 3, details: 'Sushi', type: 'Normal' }
+      ];
+    }
+  };
+  </script>
+  
