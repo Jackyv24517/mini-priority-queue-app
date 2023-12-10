c5d0d6a4502ddd5d3dd98f6aec16349ad13607f1
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-11 06:14:57 +0800

    refactor: updated display order by types

--- frontend/components/DisplayOrder.vue
+++ frontend/components/DisplayOrder.vue
@@ -1,47 +1,53 @@
 <template>
-    <v-container>
-      <h2>Pending Orders</h2>
-      <v-list>
-        <v-list-item 
-          v-for="order in pendingOrders" 
-          :key="order.id"
-        >
-          {{ order.details }} - {{ order.type }}
-        </v-list-item>
-      </v-list>
-  
-      <h2>Completed Orders</h2>
-      <v-list>
-        <v-list-item 
-          v-for="order in completedOrders" 
-          :key="order.id"
-        >
-          {{ order.details }} - {{ order.type }}
-        </v-list-item>
-      </v-list>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        pendingOrders: [],
-        completedOrders: []
-      };
-    },
-    mounted() {
-      // Fetch orders from the backend and populate pendingOrders and completedOrders
+  <v-container>
+    <h2>Pending Orders</h2>
+    <v-list>
+      <v-list-item 
+        v-for="order in pendingOrders" 
+        :key="order.orderId"
+      >
+        Order #{{ order.orderId }} - {{ order.type }}
+      </v-list-item>
+    </v-list>
 
-      // mock template, needed to replace with actual API call
-      this.pendingOrders = [
-        { id: 1, details: 'Pizza', type: 'Normal' },
-        { id: 2, details: 'Burger', type: 'VIP' }
-      ];
-      this.completedOrders = [
-        { id: 3, details: 'Sushi', type: 'Normal' }
-      ];
+    <h2>Completed Orders</h2>
+    <v-list>
+      <v-list-item 
+        v-for="order in completedOrders" 
+        :key="order.orderId"
+      >
+        Order #{{ order.orderId }} - {{ order.type }}
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pendingOrders: [],
+      completedOrders: []
+    };
+  },
+  methods: {
+    async fetchOrders() {
+      try {
+        // Replace with actual API call
+        const response = await this.$axios.get('/orders');
+        const orders = response.data;
+
+        // Assuming the backend sends orders categorized by status
+        this.pendingOrders = orders.filter(order => order.status === 'PENDING');
+        this.completedOrders = orders.filter(order => order.status === 'COMPLETE');
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+        // Handle error
+      }
     }
-  };
-  </script>
-  
+  },
+  mounted() {
+    this.fetchOrders();
+  }
+};
+</script>
