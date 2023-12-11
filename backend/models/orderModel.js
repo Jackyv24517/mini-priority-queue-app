cec26d337d8fc135e2317ae09b896f1a4af41266
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-11 21:31:41 +0800

    feat: order creation backend logic and schema

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,9 +1,41 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  orderId: Number,
-  type: String, // 'Normal' or 'VIP'
-  status: String // 'PENDING', 'PROCESSING', 'COMPLETE'
+  // Unique order identifier
+  orderId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+
+  // Type of order (e.g., 'Normal', 'VIP')
+  type: {
+    type: String,
+    required: true,
+    enum: ['Normal', 'VIP']
+  },
+
+  // Status of the order (e.g., 'PENDING', 'COMPLETED')
+  status: {
+    type: String,
+    required: true,
+    enum: ['PENDING', 'PROCESSING', 'COMPLETED'],
+    default: 'PENDING'
+  },
+
+  // Timestamp for when the order was created
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+
+  // Additional details (if needed)
+  details: {
+    type: String,
+    default: ''
+  }
+
+  // You can add more fields as required by your application
 });
 
 module.exports = mongoose.model('Order', orderSchema);
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,16 +1,54 @@
 const express = require('express');
-const Order = require('../models/order.model');
+const Order = require('../models/orderModel');
+const Counter = require('./models/counterModel');
 const router = express.Router();
 
-let nextOrderId = 1; // Simulates an auto-incrementing order ID
+// POST /api/orders - Create a new order
+router.post('/orders', async (req, res) => {
+  try {
+    const { type, details } = req.body;
 
-router.post('/create', async (req, res) => {
-  const { type } = req.body; // 'Normal' or 'VIP'
-  const newOrder = new Order({ orderId: nextOrderId++, type, status: 'PENDING' });
-  await newOrder.save();
-  res.status(201).json(newOrder);
+    // Generate the next order ID here (you'll need to implement this logic)
+    const nextOrderId = await getNextOrderId();
+
+    const newOrder = new Order({
+      orderId: nextOrderId,
+      type,
+      details
+    });
+
+    await newOrder.save();
+    res.status(201).json(newOrder);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-// Add other routes as needed for updating and managing orders
+async function getNextOrderId(orderType) {
+  // Check and initialize the counter if not present
+  let counter = await Counter.findById('order');
+  if (!counter) {
+    counter = new Counter({ _id: 'order', seq: 1 });
+    await counter.save();
+  }
+
+  // Increment the counter
+  counter = await Counter.findByIdAndUpdate('order', { $inc: { seq: 1 } }, { new: true });
+
+  // Format the order ID based on the order type
+  const prefix = orderType === 'VIP' ? 'VIP-' : 'N-';
+  return prefix + counter.seq;
+}
+
+
+/*
+ Description on logic
+
+Initialization Check: The function first checks if a counter document exists in the database for orders. If not, it creates one and initializes it with a sequence number of 1.
+
+Incrementing the Counter: The counter's sequence number is then atomically incremented using findByIdAndUpdate. This ensures that each order gets a unique ID, even in the case of concurrent requests.
+
+ID Formatting: Finally, the function formats the order ID by prepending a type-specific prefix ('VIP-' for VIP orders and 'N-' for Normal orders) to the incremented sequence number.
+ */
 
 module.exports = router;
