2c3d3d1d2ba3af3e040dc317147dd37166c46f7e
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-12 15:10:19 +0800

    feat: dashboard on displaying order via socketIO

--- frontend/components/DisplayOrder.vue
+++ frontend/components/DisplayOrder.vue
@@ -1,53 +1,74 @@
 <template>
   <v-container>
-    <h2>Pending Orders</h2>
-    <v-list>
-      <v-list-item 
-        v-for="order in pendingOrders" 
-        :key="order.orderId"
-      >
-        Order #{{ order.orderId }} - {{ order.type }}
-      </v-list-item>
-    </v-list>
+    <v-row>
+      <!-- Pending Orders Column -->
+      <v-col cols="12" md="6">
+        <h2>Pending Orders</h2>
+        <v-list>
+          <v-list-item 
+            v-for="order in pendingOrders" 
+            :key="order.orderId"
+          >
+            Order #{{ order.orderId }} - {{ order.type }}
+          </v-list-item>
+        </v-list>
+      </v-col>
 
-    <h2>Completed Orders</h2>
-    <v-list>
-      <v-list-item 
-        v-for="order in completedOrders" 
-        :key="order.orderId"
-      >
-        Order #{{ order.orderId }} - {{ order.type }}
-      </v-list-item>
-    </v-list>
+      <!-- Completed Orders Column -->
+      <v-col cols="12" md="6">
+        <h2>Completed Orders</h2>
+        <v-list>
+          <v-list-item 
+            v-for="order in completedOrders" 
+            :key="order.orderId"
+          >
+            Order #{{ order.orderId }} - {{ order.type }}
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
+
 <script>
+import io from 'socket.io-client';
+
 export default {
   data() {
     return {
       pendingOrders: [],
-      completedOrders: []
+      completedOrders: [],
+      socket: null,
     };
   },
   methods: {
     async fetchOrders() {
-      try {
-        // Replace with actual API call
-        const response = await this.$axios.get('/orders');
-        const orders = response.data;
-
-        // Assuming the backend sends orders categorized by status
-        this.pendingOrders = orders.filter(order => order.status === 'PENDING');
-        this.completedOrders = orders.filter(order => order.status === 'COMPLETE');
-      } catch (error) {
-        console.error('Error fetching orders:', error);
-        // Handle error
+      // Existing fetch logic...
+    },
+    handleOrderUpdate(updatedOrder) {
+      // Logic to handle an updated order
+      // Add to completedOrders if status is 'COMPLETE', else to pendingOrders
+      if (updatedOrder.status === 'COMPLETE') {
+        this.completedOrders.push(updatedOrder);
+      } else {
+        this.pendingOrders.push(updatedOrder);
       }
     }
   },
-  mounted() {
+  created() {
     this.fetchOrders();
+
+    // Initialize WebSocket connection
+    this.socket = io('http://localhost:3000'); // Replace with your server URL
+    this.socket.on('orderUpdate', (order) => {
+      this.handleOrderUpdate(order);
+    });
+  },
+  beforeDestroy() {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
   }
 };
 </script>
---
+++ frontend/pages/dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+      <DisplayOrder />
+    </v-container>
+  </template>
+  
+  <script>
+  import DisplayOrder from '@/components/DisplayOrder';
+  
+  export default {
+    components: {
+      DisplayOrder,
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
+  
