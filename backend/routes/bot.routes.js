4ec1836567d8e3e9f705a2acf3d5603a3de715d0
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-12 19:51:30 +0800

    refactor: remove bot method for in-memory

--- backend/routes/bot.routes.js
+++ backend/routes/bot.routes.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const botStore = require('../store/botStore');
 
 // In-memory storage for bots
-let bots = [];
-let nextBotId = 1;
+let bots = botStore.getBots;
+//let nextBotId = 1;
 
 // Add new bot
 router.post('/bots', (req, res) => {
   try {
+    botStore.addBot({ status: 'IDLE' });
+    /*
     const newBot = {
       botId: nextBotId++,
       status: 'IDLE'  // Initial bots status as 'IDLE'
-    };
 
-    bots.push(newBot);
+      bots.push(newBot);
+    };*/
+
+    
     res.status(201).json(newBot);
   } catch (error) {
     res.status(500).json({ message: error.toString() });
@@ -31,19 +36,23 @@ router.get('/bots', (req, res) => {
 
 // Remove a bot
 router.delete('/bots/:botId', (req, res) => {
-  try {
-    const botId = parseInt(req.params.botId);
-    const botIndex = bots.findIndex(bot => bot.botId === botId);
-    if (botIndex === -1) {
-      return res.status(404).json({ message: 'Bot not found' });
+    try {
+      const botId = parseInt(req.params.botId);
+  
+      // Check if the bot exists
+      const bots = botStore.getBots();
+      const botExists = bots.some(bot => bot.botId === botId);
+      if (!botExists) {
+        return res.status(404).json({ message: 'Bot not found' });
+      }
+  
+      // Remove the bot using botStore
+      botStore.removeBot(botId);
+  
+      res.json({ message: 'Bot removed' });
+    } catch (error) {
+      res.status(500).json({ message: error.toString() });
     }
-
-    // Remove the bot from the array
-    bots.splice(botIndex, 1);
-    res.json({ message: 'Bot removed' });
-  } catch (error) {
-    res.status(500).json({ message: error.toString() });
-  }
-});
+  });
 
 module.exports = router;
