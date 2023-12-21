34c851cb0e0b169feb728608f13b5f54d8eb8dd3
Jackyv24517 <62867927+Jackyv24517@users.noreply.github.com>
2023-12-21 09:10:40 +0800

    feat: in-memory for user login and register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,21 +1,50 @@
-// authController.js
-const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-//user login auth
+let users = []; // Array to store user information
+
+const register = async (req, res) => {
+  try {
+    const { username, password, roles } = req.body;
+
+    // Check if user already exists in the in-memory store
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+      return res.status(400).send('User already exists');
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create a new user object
+    const newUser = {
+      id: users.length + 1, // Simple ID assignment
+      username,
+      passwordHash,
+      roles: roles || ['User']
+    };
+    users.push(newUser);
+
+    // Respond with success message
+    res.status(201).send('User registered successfully');
+  } catch (error) {
+    res.status(500).send('Error registering new user');
+  }
+};
+
 const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    const user = users.find(user => user.username === username);
     if (!user || !await bcrypt.compare(password, user.passwordHash)) {
       return res.status(401).send('Invalid username or password');
     }
 
     const token = jwt.sign(
       { 
-        _id: user._id, 
-        roles: user.roles  // Add the role here
+        id: user.id, 
+        roles: user.roles
       },
       process.env.JWT_SECRET,
       { expiresIn: '1h' }
@@ -26,44 +55,5 @@ const login = async (req, res) => {
   }
 };
 
-
-//user register
-const register = async (req, res) => {
-    try {
-      const { username, password, roles } = req.body; // Include 'roles' in the destructuring
-  
-      // Check if user already exists
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-        return res.status(400).send('User already exists');
-      }
-  
-      // Hash the password
-      const salt = await bcrypt.genSalt(10);
-      const passwordHash = await bcrypt.hash(password, salt);
-  
-      // Create a new user with roles
-      // Ensure roles are assigned appropriately, whether default or from the request
-      const user = new User({
-        username,
-        passwordHash,
-        roles: roles || ['User'] // Default role to 'User' if not provided
-      });
-      await user.save();
-  
-      // Create a token that includes the roles in its payload
-      const token = jwt.sign({
-        _id: user._id,
-        roles: user.roles
-      }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      
-      //instead of auto login after successful registation
-      //res.status(201).send({ token });
-      res.status(201).send('User registered successfully');
-    } catch (error) {
-      res.status(500).send('Error registering new user');
-    }
-};
-
-
 module.exports = { login, register };
+
